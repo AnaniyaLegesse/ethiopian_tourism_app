e264e9a1c0262a4b2cc48dec4add1dc6ffa3c173
AnaniyaLegesse <ananiya.legesse1@gmail.com>
2024-10-04 13:31:17 +0300

    Geting started

---
+++ src/pages/About.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { MoveLeft } from 'lucide-react'
+
+const About = () => {
+  return (
+    <Container>
+      <Link to="/">
+              <button className='outline_btn'><MoveLeft size={15}/>Back to Home</button>
+      </Link>
+      <div>
+      <div className='content'>
+            <p>We specialize in providing unforgettable experiences in Ethiopia,
+            from breathtaking landscapes to rich cultural heritage.</p>
+            
+        </div>
+        <div className='image'>
+            <img src='/background.jpg' alt=""/>
+        </div>
+      </div>
+        
+        
+    </Container>
+  )
+}
+
+export default About
+
+const Container=styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.5;
+  padding:1rem;
+ 
+.outline_btn{
+  background-color: transparent;
+  color:#1760ff;
+  border: 1px solid #1760ff;
+  }
+
+ div{
+  display:flex;
+  flex-wrap: wrap;
+  justify-content: center; 
+  align-items: center;  
+ }
+  .content {
+    flex: 1;               
+    text-align: left;      
+    padding-left: 2rem; 
+
+    p{
+        font-size: 2.2rem;
+        line-height: 1.3;
+    } 
+  }
+
+  .image {
+    flex: 1;               
+    display: flex;
+    justify-content: center; 
+    align-items: center;     
+    padding:1rem;
+   
+    img {
+    max-width: 100%;
+    height: auto;
+    border-radius: 0.5rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column; /* Stack the content and image vertically */
+    .content, .image {
+      flex: unset; /* Remove flex behavior */
+      width: 100%; /* Ensure both content and image take full width */
+      padding: 1rem; /* Add some padding for breathing space */
+    }
+
+    .content {
+      padding-left: 1rem; /* Reduce padding for smaller screens */
+      text-align: center; /* Center text for better presentation */
+      p {
+        font-size: 1.5rem; /* Reduce font size for smaller screens */
+      }
+    }
+
+    .image {
+      padding: 0; /* Remove extra padding for images */
+    }
+  }
+ 
+ `
---
+++ src/pages/Home.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { MoveRight } from 'lucide-react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Home = () => {
+  return (
+    <Container>
+        <div className='content'>
+            <h1>Explore Ethiopia</h1>
+            <Link
+                to="/about"
+            >
+            <button>Learn More  <MoveRight size={15}/></button>
+            </Link>
+            
+        </div>
+     
+    </Container>
+  )
+}
+
+export default Home
+
+const Container = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.5;
+ 
+  color: rgba(255, 255, 255, 0.87);
+  background-image: linear-gradient(to bottom, rgba(129, 129, 129, 0.1), rgba(18, 18, 18, 0.8)), url('/background.jpg');
+  background-size: cover;
+  background-position: center;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  .content{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    min-width: 100vw;
+  }
+h1 {
+  font-size: 5.2em;
+  line-height: 1.1;
+}  
+`
